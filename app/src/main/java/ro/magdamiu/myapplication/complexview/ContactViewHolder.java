53231b246bff43aa3cf04106fbfaacaa98f563c8
magdamiu <badita.magda@gmail.com>
2019-02-16 17:17:27 +0200

    RecyclerView and ConstraintLayout examples

---
+++ app/src/main/java/ro/magdamiu/myapplication/complexview/ContactViewHolder.java
@@ -0,0 +1,58 @@
+package ro.magdamiu.myapplication.complexview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import ro.magdamiu.myapplication.R;
+
+public class ContactViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView mTextViewFullName;
+    private TextView mTextViewAddres;
+    private TextView mTextViewGroup;
+    private ImageView mImageViewProfilePicture;
+
+    public ContactViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        mTextViewFullName = itemView.findViewById(R.id.text_view_full_name);
+        mTextViewAddres = itemView.findViewById(R.id.text_view_address);
+        mTextViewGroup = itemView.findViewById(R.id.text_view_group);
+        mImageViewProfilePicture = itemView.findViewById(R.id.image_view_profile_picture);
+    }
+
+    public TextView getTextViewFullName() {
+        return mTextViewFullName;
+    }
+
+    public void setTextViewFullName(TextView textViewFullName) {
+        mTextViewFullName = textViewFullName;
+    }
+
+    public TextView getTextViewAddres() {
+        return mTextViewAddres;
+    }
+
+    public void setTextViewAddres(TextView textViewAddres) {
+        mTextViewAddres = textViewAddres;
+    }
+
+    public TextView getTextViewGroup() {
+        return mTextViewGroup;
+    }
+
+    public void setTextViewGroup(TextView textViewGroup) {
+        mTextViewGroup = textViewGroup;
+    }
+
+    public ImageView getImageViewProfilePicture() {
+        return mImageViewProfilePicture;
+    }
+
+    public void setImageViewProfilePicture(ImageView imageViewProfilePicture) {
+        mImageViewProfilePicture = imageViewProfilePicture;
+    }
+}
---
+++ app/src/main/java/ro/magdamiu/myapplication/complexview/RecyclerViewActivity.java
@@ -0,0 +1,51 @@
+package ro.magdamiu.myapplication.complexview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ro.magdamiu.myapplication.MainActivity;
+import ro.magdamiu.myapplication.R;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+    private static final int CONTACTS_COUNT = 25;
+
+    private RecyclerView mRecyclerViewContacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+
+        initView();
+    }
+
+    private void initView() {
+        mRecyclerViewContacts = findViewById(R.id.recycler_view_contacts);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerViewContacts.setLayoutManager(layoutManager);
+
+        ContactAdapter contactAdapter = new ContactAdapter(getContacts(), RecyclerViewActivity.this);
+        mRecyclerViewContacts.setAdapter(contactAdapter);
+    }
+
+    private List<Contact> getContacts() {
+        List<Contact> contacts = new ArrayList<>();
+        Contact contact;
+        for (int i = 0; i < CONTACTS_COUNT; i++) {
+            contact = new Contact();
+            contact.setFullName("Android Name " + i);
+            contact.setAddresse("Address " + i);
+            contact.setGroup("Family " + i);
+            contact.setImageUrl("https://cdn.guidingtech.com/media/assets/WordPress-Import/2012" +
+                    "/10/Smiley-Thumbnail.png");
+            contacts.add(contact);
+        }
+        return contacts;
+    }
+}
